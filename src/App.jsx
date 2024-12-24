d9a430074fecfc7ff1894a810fb249f453d7f6f9
syunado <bennettbros68@gmail.com>
2024-12-23 23:58:13 -0500

    sections 1-2 done on linked in learning, and this is the website result

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,57 @@
+import React from "react";
+import './App.css';
+import smokehouseBurger from "./assets/foodimages/smokehouse_burger.png";
 
-import './App.css'
 
-function Header() {
+
+function Header({ name, year }) {
   return (
     <header>
-      <h1>Ty's Big Ball Lair</h1>
+      <h1>Carolina Wings </h1>
+      <p>Copyright {year}</p>
     </header>
-  )
+  );
+}
+
+const items = [
+  "Mac n cheese",
+  "Salmon ",
+  "Smokehouse Burger"
+];
+
+const itemsObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
+
+function Main({ dishes }) {
+
+  return (
+    <>
+    <div>
+      <h2>Welcome to caorlina wings</h2>
+    </div>
+    
+    <main>
+      <img src={smokehouseBurger} height={300} width="auto"/>
+      <ul>
+        {dishes.map((dish, i) => (
+          <li key={dish.id} style={{ listStyleType:"none"}}>{dish.title}</li>
+        ))}
+      </ul>
+    </main>
+    </>
+  );
 }
 
 function App() {
 
   return (
-    <Header />
-  ) 
+    <div>
+    <Header name="Ty" year={new Date().getFullYear()}/>
+    <Main dishes={itemsObjects}/>
+    </div>
+  );
 }
 
 export default App
