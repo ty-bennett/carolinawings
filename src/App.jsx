6e8c2ac33ec87dc899f542245cfecb568f02ed31
syunado <bennettbros68@gmail.com>
2024-12-23 19:37:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function Header() {
+  return (
+    <header>
+      <h1>Ty's Big Ball Lair</h1>
+    </header>
+  )
+}
+
+function App() {
+
+  return (
+    <Header />
+  ) 
+}
+
+export default App
