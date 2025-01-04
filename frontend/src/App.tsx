22bdff1d73730ecb239c6b171d220d3cb1cef38c
syunado <bennettbros68@gmail.com>
2025-01-04 01:26:11 -0500

    first commit for cws website, redoing in ts

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+
+function NavBar() {
+  return (
+    <main>
+      <div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Lunch Specials</li>
+            <li>Nightly Specials</li>
+            <li>Catering</li>
+            <li>Locations</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </main>
+  )
+}
+
+export default NavBar;
