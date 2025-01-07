62a1fa3c4ce8a52de26ee3bc3dc40f76955a1fcb
syunado <bennettbros68@gmail.com>
2025-01-06 23:32:57 -0500

    nav bar almost done, just have to figure out positioning, then onto the next section!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import NavBar from './components/NavBar.tsx'
 
 
-
 function App() {
   return (
   <>
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,21 +1,30 @@
 import navbar from './NavBar.module.css';
-import Link from 'react-dom'
+import wood from '/backgroundImages/woodtexture.jpg'
+import logo from '/carolinawingslogo.png'
 
 function NavBar() {
+  const isActive = true;
   return (
     <main>
-      <div>
-        <nav>
-          <ul className={navbar.navbar}>
-            <li className={navbar.navbaritem}>Home</li>
-            <li className={navbar.navbaritem}>Menu</li>
-            <li className={navbar.navbaritem}>Lunch Specials</li>
-            <li className={navbar.navbaritem}>Nightly Specials</li>
-            <li className={navbar.navbaritem}>Catering</li>
-            <li className={navbar.navbaritem}>Locations</li>
-            <li className={navbar.navbaritem}>Contact</li>
-          </ul>
-        </nav>
+      <div className={navbar.stickydiv}>
+        <div className={navbar.flexcontainer}>
+          <img src={logo} className={navbar.logo}></img>
+          <img src={wood} className={navbar.img}></img>
+          <button className={navbar.ordernow}>Order Now</button>
+        </div>
+        <div className={navbar.stickydiv}>
+          <nav>
+            <ul className={navbar.navbar}>
+              <li className={(isActive ? navbar.navbaritemactive : navbar.navbaritem)}>Home</li>
+              <li className={navbar.navbaritem}>Menu</li>
+              <li className={navbar.navbaritem}>Lunch Specials</li>
+              <li className={navbar.navbaritem}>Nightly Specials</li>
+              <li className={navbar.navbaritem}>Catering</li>
+              <li className={navbar.navbaritem}>Locations</li>
+              <li className={navbar.navbaritem}>Contact</li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </main>
   )
