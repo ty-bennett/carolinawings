cf21e85bb35c9dcfd7a253d97f81a9a722b5618a
Ty Bennett <60624999+ty-bennett@users.noreply.github.com>
2025-01-08 21:22:01 +0000

    got everything lined up, fixing bg image later when I am at home, and then continuing nav bar and then finally onto the next compononent

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,8 +2,13 @@ import navbar from './NavBar.module.css';
 import wood from '/backgroundImages/woodtexture.jpg'
 import logo from '/carolinawingslogo.png'
 
-function NavBar() {
-  const isActive = true;
+interface NavBarProps {
+  isActive?: boolean;
+}
+
+
+const NavBar: React.FC<NavBarProps> = ({ isActive }) =>  {
+  isActive = true;
   return (
     <main>
       <div className={navbar.gridparent}>
@@ -11,23 +16,25 @@ function NavBar() {
           <img src={logo} className={navbar.logo}></img>
         </div>
         <div className={navbar.gridchildimg}>
-          <img src={wood} className={navbar.img}></img>
+          <img src={wood} className={navbar.backgroundimg}></img>
         </div>
         <div className={navbar.gridchildbutton}>
           <button className={navbar.ordernow}>Order Now</button>
         </div>
-        <div className={navbar.gridchild2}>
-          <nav>
-            <ul className={navbar.navbar}>
-              <li className={(isActive ? navbar.navbaritemactive : navbar.navbaritem)}>Home</li>
-              <li className={navbar.navbaritem}>Menu</li>
-              <li className={navbar.navbaritem}>Lunch Specials</li>
-              <li className={navbar.navbaritem}>Nightly Specials</li>
-              <li className={navbar.navbaritem}>Catering</li>
-              <li className={navbar.navbaritem}>Locations</li>
-              <li className={navbar.navbaritem}>Contact</li>
-            </ul>
-          </nav>
+        <div className={navbar.gridchildnav}>
+          <div className={navbar.navbarposition}>
+            <nav>
+              <ul className={navbar.navbar}>
+                <li className={({isActive} ? navbar.navbaritemactive : navbar.navbaritem)}>Home</li>
+                <li className={navbar.navbaritem}>Menu</li>
+                <li className={navbar.navbaritem}>Lunch Specials</li>
+                <li className={navbar.navbaritem}>Nightly Specials</li>
+                <li className={navbar.navbaritem}>Catering</li>
+                <li className={navbar.navbaritem}>Locations</li>
+                <li className={navbar.navbaritem}>Contact</li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </main>
