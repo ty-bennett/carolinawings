c39fc05c2479bf2338e32502ae561cf7da0f4e7e
ty-bennett <bennettbros68@gmail.com>
2025-01-10 23:52:39 -0500

    finally got everything squared away, I will worry about how it looks when resizing later, onto next component which is solid background that pans when you scroll, and also some buttons that link to different headers later in the page, and then lastly ill do the app routing for different pages like specials and whatnot.

---
+++ frontend/src/components/GoodEats.tsx
@@ -0,0 +1,6 @@
+//TODO: 
+// background image that stays when you scroll
+// floating white div that scrolls down to location component
+//  specials div that links to specials page
+
+import goodeats from './GoodEats.module.css';
