ccb283f4deeab2654cd1caaddbfeccee51e8dbdb
Ty Bennett <60624999+ty-bennett@users.noreply.github.com>
2025-01-14 21:09:14 +0000

    final changes before class, going to work on this tonight or tomorrow morning, progress is coming along good, shouldn't be too hard to get the fixed image to work, should just be some basic css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import NavBar from './components/NavBar.tsx'
-
+import GoodEats from './components/GoodEats.tsx'
 
 function App() {
   return (
   <>
     <NavBar /> 
+    <GoodEats />
   </>
   );
 }
