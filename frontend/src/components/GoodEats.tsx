a33071d6c717a1362865a96d6466f0c53c64a568
ty-bennett <bennettbros68@gmail.com>
2025-01-15 23:53:52 -0500

    stopping here for today, figured out parallax scrolling just need to make a few styling changes, which I will do tomorrow, headed to bed for now though, have a meeting in the AM and class! -Ty

--- frontend/src/components/GoodEats.tsx
+++ frontend/src/components/GoodEats.tsx
@@ -9,10 +9,16 @@ import goodeats from './GoodEats.module.css';
 function GoodEats() {
     return (
         <div className={goodeats.bgimg}>
-            <div className={goodeats.floatingdiv}>
-                <h1 className={goodeats.goodeatsheading}>Good Eats</h1>
-                <h3 className={goodeats.subheader}>Carolina Wings & Rib House</h3>
-                <button className={goodeats.chooselocationbutton}>Choose Location</button>
+            <div className={goodeats.container}>
+                <div className={goodeats.floatingdiv}>
+                    <div>
+                        <h1 className={goodeats.goodeatsheading}>Good Eats</h1>
+                    </div>
+                    <div>
+                        <h3 className={goodeats.subheader}>Carolina Wings & Rib House</h3>
+                    </div>
+                    <button className={goodeats.chooselocationbutton}>Choose Location</button>
+                </div>
             </div>
         </div>
     )
