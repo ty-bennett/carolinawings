c052f7ec9139d899d27f8109fcab98e8b9c441f4
Ty Bennett <tymb@email.sc.edu>
2025-01-23 16:24:39 -0500

    1/4 done with sauces

--- frontend/src/components/Sauces.tsx
+++ frontend/src/components/Sauces.tsx
@@ -9,16 +9,30 @@ function Sauces() {
       <div className={sauces.contentcontainer}>
         <div className={sauces.cardcontainer}>
           <div className={sauces.card}>
-            <p className={sauces.text}>Balls</p>
+            <h1 className={sauces.sauceheader}>Sauces</h1>
+            <div className={sauces.saucecontainer}>
+              <p className={sauces.text}> MILD </p>
+              <p className={sauces.text}> MILD HONEY </p>
+              <p className={sauces.text}> BBQ</p>
+              <p className={sauces.text}> TERIYAKI </p>
+              <p className={sauces.text}> TERI-BBQ </p>
+              <p className={sauces.text}> DOC'S BBQ </p>
+              <p className={sauces.text}> HONEY BBQ </p>
+              <p className={sauces.text}> HOT GARLIC </p>
+              <p className={sauces.text}> HONEY MUSTARD </p>
+              <p className={sauces.text}> GARLIC PARMESAN </p>
+              <p className={sauces.text}> MANGO HABANERO </p>
+            </div>
           </div>
           <div className={sauces.card}>
-            <p className={sauces.text}>Balls</p>
+            <p className={sauces.text}>🌶text</p>
           </div>
           <div className={sauces.card}>
-            <p className={sauces.text}>Balls</p>
+            <p className={sauces.text}>🌶text</p>
           </div>
           <div className={sauces.card}>
-            <p className={sauces.text}>Balls</p>
+          <h1 className={sauces.sauceheader}>Spice Meter</h1>
+            <p className={sauces.text}>🌶text</p>
           </div>
         </div>
       </div>
