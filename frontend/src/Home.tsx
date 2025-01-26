6bf3b8ca108ec5f8c47f3a3c32fda418f052ed47
Ty Bennett <tymb@email.sc.edu>
2025-01-25 22:41:19 -0500

    done with gift card component, map component now, itll just be an iframe

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -4,6 +4,7 @@ import GoodEats from './components/GoodEats.tsx'
 import Locations from './components/Locations.tsx'
 import About from './components/About.tsx'
 import Sauces from './components/Sauces.tsx'
+import GiftCard from './components/GiftCard.tsx'
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
     <Locations />
     <About />
     <Sauces />
+    <GiftCard />
   </>
   );
 }
