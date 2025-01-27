67526ae64d4d59fe37423c73aea7c0ecfff7ea83
Ty Bennett <tymb@email.sc.edu>
2025-01-27 08:47:46 -0500

    footer update

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,17 +5,20 @@ function Footer() {
     <div className={footer.footercontainer}>
       <div className={footer.locationcontainer}>
         <div className={footer.location}>
-          <h1>Ballentine</h1>
+          <h1 className={footer.header}><u>Ballentine</u></h1>
         </div>
         <div className={footer.location}>
-          <h1>Lexington</h1>
+          <h1 className={footer.header}><u>Lexington</u></h1>
         </div>
         <div className={footer.location}>
-          <h1>Cayce</h1>
+          <h1 className={footer.header}><u>Cayce</u></h1>
         </div>
         <div className={footer.location}>
-          <h1>Red Bank</h1>
+          <h1 className={footer.header}><u>Red Bank</u></h1>
         </div>
+      </div>
+      <div>
+        <img src="/carolinawings.png" className={footer.image}></img>
       </div>
     </div>
   );
