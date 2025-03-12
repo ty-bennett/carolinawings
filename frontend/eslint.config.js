416849463dec7572af62823d200d0aa932bc5e25
Ty Bennett <60624999+ty-bennett@users.noreply.github.com>
2025-03-12 15:47:36 +0000

    resolved merge conflicts

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,18 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-<<<<<<< HEAD
-import tseslint from 'typescript-eslint'
-
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-=======
 
 export default [
   { ignores: ['dist'] },
@@ -27,28 +15,19 @@ export default [
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
->>>>>>> origin/main
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-<<<<<<< HEAD
-      ...reactHooks.configs.recommended.rules,
-=======
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
->>>>>>> origin/main
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-<<<<<<< HEAD
-)
-=======
 ]
->>>>>>> origin/main
