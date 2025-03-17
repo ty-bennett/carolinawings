5828b373b1f62c21e6da5552c8871c05c39966cc
Ty Bennett <tymb@email.sc.edu>
2025-03-17 14:14:32 -0400

    last commit before end of break

--- frontend/src/components/MenuItem.jsx
+++ frontend/src/components/MenuItem.jsx
@@ -1,10 +1,15 @@
-
+import menuItem from "../components/MenuItem.module.css";
 
 const MenuItem = ({isFavorite, isVegetarian, name, description, price, periods }) =>
 {
   return(
-    <div>
-      {isFavorite ? <img src=""/> : }
+    <div className={menuItem.cardcontainer}>
+      {isFavorite ? <img src="yellow_heart.png" width="64" height="64"/> : <img src=""/>}
+      {isVegetarian ?
+        <span>Ⓥ</span> : <span></span>}
+      <span>{name.toUpperCase()}</span>
     </div>
   );
 }
+
+export default MenuItem;
--- frontend/src/components/MenuSection.jsx
+++ frontend/src/components/MenuSection.jsx
@@ -1,12 +1,27 @@
 import MenuItem from "../components/MenuItem"
+import section from "../components/MenuSection";
 
 const MenuSection = () => {
   return(
-    <MenuItem isFavorite={true}
-              isVegetarian={false}
-              name={"BUFFALO SHRIMP"}
-              description={"Our famous hand-battered shrimp tossed in our award-winning buffalo sauce. Served with bleu cheese or ranch dressing."}
-              price={9.99} />
+    <>
+      <div className={section.contentcontainer}> 
+        <h1 className={section.title}>Shareables</h1>
+        <div className={section.rowcontainer}>   
+          <MenuItem isFavorite={true}
+                    isVegetarian={false}
+                    name={"BUFFALO SHRIMP"}
+                    description={"Our famous hand-battered shrimp tossed in our award-winning buffalo sauce. Served with bleu cheese or ranch dressing."}
+                    price={9.99} 
+          />
+          <MenuItem isFavorite={false}
+                    isVegetarian={false}
+                    name={"QUESADILLA"}
+                    descripton={["Tomato basil tortilla with three cheese served with your choice of", <span>southwest mix, chicken, brisket or cheese</span>]}
+                    price={10.99}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
