6cbe8d29c7d311880eed6553b520f006a48b3bc9
ty-bennett <bennettbros68@gmail.com>
2025-05-09 00:15:23 -0400

    added a root get mapping

--- backend/src/main/java/com/carolinawings/webapp/OrderSystemApplication.java
+++ backend/src/main/java/com/carolinawings/webapp/OrderSystemApplication.java
@@ -2,13 +2,19 @@ package com.carolinawings.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OrderSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderSystemApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home() {
+		return "/frontend/index.html";
+	}
 }
