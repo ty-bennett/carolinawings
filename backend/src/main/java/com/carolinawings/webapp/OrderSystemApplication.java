cb64c8fb10f79288fcbcb0353435c88be23fb1af
ty-bennett <bennettbros68@gmail.com>
2025-05-11 23:45:22 -0400

    idk whatever work i did recently, still figuring out how to setup project

--- backend/src/main/java/com/carolinawings/webapp/OrderSystemApplication.java
+++ backend/src/main/java/com/carolinawings/webapp/OrderSystemApplication.java
@@ -6,15 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class OrderSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderSystemApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String home() {
-		return "/frontend/index.html";
-	}
 }
